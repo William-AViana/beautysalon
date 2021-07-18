f89c5865e4b5a2a142476d3af1275f862516752d
William Viana <willviana.batera@gmail.com>
2021-07-18 16:06:09 -0300

    first commit on aula4

--- main.js
+++ main.js
@@ -15,16 +15,16 @@ for (const link of links) {
     })
 }
 
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+    const header = document.querySelector('#header')
+    const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
     if (window.scrollY >= navHeight) {
         header.classList.add('scroll')
     } else {
         header.classList.remove('scroll')
     }
-})
+}
 
 // Testimonials carosel slider swiper
 const swiper = new Swiper('.swiper-container', {
@@ -50,6 +50,23 @@ scrollReveal.reveal(
     #about .image, #about .text,
     #services header, #services .card,
     #testimonials .text, #testimonials .testimonials,
-    #contact .text, #contact .links`,
+    #contact .text, #contact .links,
+    footer .brand, footer .social`,
     { interval: 100 }
 )
+
+/* buttom back to top */
+function backToTop() {
+    const backToTopButton = document.querySelector('.back-to-top')
+    if (window.scrollY >= 560) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
+}
+
+/* When scroll */
+window.addEventListener('scroll', function () {
+    changeHeaderWhenScroll()
+    backToTop()
+})
